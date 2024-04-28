6530f5539ba716312b6e4ba1cb9f74dac1641106
Crintsux <fifa.d@seznam.cz>
2024-04-28 17:02:20 +0200

    Started impleneting ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,4 +17,18 @@ class HTMLNode:
         return output
     
     def __repr__(self):
-        return f"Tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props}"
+        return f"HTMLNode(Tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode(Tag: {self.tag}, value: {self.value}, props: {self.props})"
