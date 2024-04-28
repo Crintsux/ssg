877ef335922faec25c97d9bea257a763a07646e6
Crintsux <fifa.d@seznam.cz>
2024-04-28 15:20:51 +0200

    HTMLNode implemented

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode:
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        output = ""
+        for key in self.props:
+            output += f' {key}="{self.props[key]}"'
+        return output
+    
+    def __repr__(self):
+        return f"Tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props}"
